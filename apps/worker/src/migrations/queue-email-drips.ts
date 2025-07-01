a909e843592cf31583fb8ed3b5a5f3f37540e7b9
Dominik Sumer <domi.sumer@gmail.com>
2025-07-01 15:08:53 +0200

    added migration for queuing email drips

---
+++ apps/worker/src/migrations/queue-email-drips.ts
@@ -0,0 +1,77 @@
+import { getDripSequence } from '@vemetric/email/email-drip-sequences';
+import { emailDripQueue } from '@vemetric/queues/email-drip-queue';
+import { addToQueue } from '@vemetric/queues/queue-utils';
+import { clickhouseEvent } from 'clickhouse';
+import { prismaClient } from 'database';
+import { logger } from '../utils/logger';
+
+async function main() {
+  const noProjectSequence = getDripSequence('NO_PROJECT');
+
+  const users = await prismaClient.user.findMany({
+    where: {
+      emailVerified: true,
+    },
+    include: {
+      projects: true,
+    },
+  });
+
+  let delayHour = 1;
+  for (const user of users) {
+    if (user.projects.length > 0) {
+      continue;
+    }
+
+    const userId = user.id;
+    logger.info(
+      { userId, queueName: emailDripQueue.name, sequence: noProjectSequence, delayHour },
+      'Queuing user for email drip sequence',
+    );
+    await addToQueue(
+      emailDripQueue,
+      {
+        userId,
+        sequenceType: noProjectSequence.type,
+        stepNumber: 0,
+      },
+      {
+        delay: delayHour * 60 * 60 * 1000,
+      },
+    );
+    delayHour++;
+  }
+
+  const noEventsSequence = getDripSequence('NO_EVENTS');
+
+  const projects = await prismaClient.project.findMany({});
+
+  for (const project of projects) {
+    const hasEvents = (await clickhouseEvent.getAllEventsCount(BigInt(project.id))) > 0;
+    if (hasEvents) {
+      continue;
+    }
+
+    logger.info(
+      { projectId: project.id, queueName: emailDripQueue.name, sequence: noEventsSequence },
+      'Queuing project for email drip sequence',
+    );
+    await addToQueue(
+      emailDripQueue,
+      {
+        projectId: project.id,
+        sequenceType: noEventsSequence.type,
+        stepNumber: 0,
+      },
+      {
+        delay: delayHour * 60 * 60 * 1000,
+      },
+    );
+    delayHour++;
+  }
+}
+
+main().catch((error) => {
+  logger.error('Queueing email drips failed:', error);
+  process.exit(1);
+});

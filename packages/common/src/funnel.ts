a1ac985e1b02cccad5481b481931e626b40357cd
Dominik Sumer <domi.sumer@gmail.com>
2025-08-07 22:55:58 +0200

    min len check for funnel step id

--- packages/common/src/funnel.ts
+++ packages/common/src/funnel.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 import { eventFilterSchema, pageFilterSchema } from './filters';
 
 export const funnelStepSchema = z.object({
-  id: z.string(),
+  id: z.string().min(3),
   name: z.string(),
   filter: z.union([pageFilterSchema, eventFilterSchema]),
 });

527d9515880fde419286a9d13fb855d80790b26d
Dominik Sumer <domi.sumer@gmail.com>
2025-08-08 08:26:08 +0200

    extracted funnel skeleton component

--- apps/webapp/src/pages/_layout/p/$projectId/funnels/$funnelId.tsx
+++ apps/webapp/src/pages/_layout/p/$projectId/funnels/$funnelId.tsx
@@ -11,6 +11,7 @@ import { FilterContextProvider } from '@/components/filter/filter-context';
 import { PageDotBackground } from '@/components/page-dot-background';
 import { ActiveUsersButton } from '@/components/pages/funnels/active-users-button';
 import { FunnelDialog } from '@/components/pages/funnels/funnel-dialog';
+import { FunnelSkeleton } from '@/components/pages/funnels/funnel-skeleton';
 import { HorizontalFunnel } from '@/components/pages/funnels/horizontal-funnel';
 import { VerticalFunnel } from '@/components/pages/funnels/vertical-funnel';
 import { TimespanSelect } from '@/components/timespan-select';
@@ -134,39 +135,7 @@ function RouteComponent() {
     return (
       <>
         <PageDotBackground />
-        <Flex pos="relative" justify="center" pt={16} animation="pulse 2s infinite">
-          <Flex align="flex-end" borderBottom="2px solid" borderColor="gray.emphasized" px="4">
-            <Box zIndex="1" h="200px" w="100px" borderTopRadius="xl" bg="gray.emphasized" />
-            <Box opacity={0.5}>
-              <Icon
-                as="svg"
-                width="50px"
-                height="195px"
-                viewBox="0 0 50 200"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                mx="-0.5"
-              >
-                <path d={`M0 0 L50 60 L50 200 L0 200 Z`} fill="var(--chakra-colors-gray-emphasized)" />
-              </Icon>
-            </Box>
-            <Box zIndex="1" h="140px" w="100px" borderTopRadius="xl" bg="gray.emphasized" />
-            <Box opacity={0.5}>
-              <Icon
-                as="svg"
-                width="50px"
-                height="195px"
-                viewBox="0 0 50 200"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                mx="-0.5"
-              >
-                <path d={`M0 60 L50 130 L50 200 L0 200 Z`} fill="var(--chakra-colors-gray-emphasized)" />
-              </Icon>
-            </Box>
-            <Box zIndex="1" h="70px" w="100px" borderTopRadius="xl" bg="gray.emphasized" />
-          </Flex>
-        </Flex>
+        <FunnelSkeleton />
       </>
     );
   }

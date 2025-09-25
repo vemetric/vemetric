dfe29bae1e503b9e5fe8cac79f0f14a6f859f3a0
Dominik Sumer <domi.sumer@gmail.com>
2025-09-25 11:56:27 +0200

    revert callbackurl

--- apps/webapp/src/utils/auth.ts
+++ apps/webapp/src/utils/auth.ts
@@ -14,6 +14,7 @@ export const loginWithProvider = async (provider: 'google' | 'github', setIsLoad
   await authClient.signIn.social(
     {
       provider,
+      callbackURL: 'https://' + window.location.hostname + '/',
     },
     {
       onRequest: () => {

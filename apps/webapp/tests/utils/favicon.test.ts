b0d146e54685fe0f9c81508adcefa22e83045f52
Dominik Sumer <domi.sumer@gmail.com>
2025-10-25 14:49:47 +0200

    fixed tests

--- apps/webapp/tests/utils/favicon.test.ts
+++ apps/webapp/tests/utils/favicon.test.ts
@@ -4,36 +4,31 @@ import { getFaviconUrl } from '../../src/utils/favicon';
 describe('getFaviconUrl', () => {
   it('should handle URLs with https protocol', () => {
     const url = 'https://example.com';
-    const expected =
-      'https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&size=64&url=https://example.com';
+    const expected = 'https://favicon.vemetric.com/https://example.com?size=64';
     expect(getFaviconUrl(url)).toBe(expected);
   });
 
   it('should handle URLs with http protocol', () => {
     const url = 'http://example.com';
-    const expected =
-      'https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&size=64&url=https://example.com';
+    const expected = 'https://favicon.vemetric.com/https://example.com?size=64';
     expect(getFaviconUrl(url)).toBe(expected);
   });
 
   it('should add https protocol to URLs without protocol', () => {
     const url = 'example.com';
-    const expected =
-      'https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&size=64&url=https://example.com';
+    const expected = 'https://favicon.vemetric.com/https://example.com?size=64';
     expect(getFaviconUrl(url)).toBe(expected);
   });
 
   it('should handle URLs with subdomains', () => {
     const url = 'https://sub.example.com';
-    const expected =
-      'https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&size=64&url=https://sub.example.com';
+    const expected = 'https://favicon.vemetric.com/https://sub.example.com?size=64';
     expect(getFaviconUrl(url)).toBe(expected);
   });
 
   it('should handle invalid URLs gracefully', () => {
     const url = 'not-a-url';
-    const expected =
-      'https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&size=64&url=https://not-a-url';
+    const expected = 'https://favicon.vemetric.com/https://not-a-url?size=64';
     expect(getFaviconUrl(url)).toBe(expected);
   });
 });

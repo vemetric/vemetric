8aa2c7becbaf9fb5f4a44dcfaebcc36d1526d7e0
Dominik Sumer <domi.sumer@gmail.com>
2025-10-25 14:57:18 +0200

    adjusted loading image logic

--- apps/webapp/src/components/loading-image.tsx
+++ apps/webapp/src/components/loading-image.tsx
@@ -1,9 +1,13 @@
 import type { ImageProps } from '@chakra-ui/react';
 import { Box, Center, Image, Skeleton } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
+import { TbWorldQuestion } from 'react-icons/tb';
 
-export const LoadingImage = (props: ImageProps) => {
+interface Props extends ImageProps {}
+
+export const LoadingImage = (props: Props) => {
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     setLoading(true);
@@ -11,12 +15,17 @@ export const LoadingImage = (props: ImageProps) => {
 
   return (
     <Box position="relative" flexShrink="0">
-      {loading && (
+      {(loading || error) && (
         <Center pos="absolute" inset="0">
-          <Skeleton boxSize="90%" />
+          {error ? <TbWorldQuestion /> : <Skeleton boxSize="90%" />}
         </Center>
       )}
-      <Image {...props} opacity={loading ? 0 : 1} onLoad={() => setLoading(false)} />
+      <Image
+        {...props}
+        opacity={loading || error ? 0 : 1}
+        onLoad={() => setLoading(false)}
+        onError={() => setError(true)}
+      />
     </Box>
   );
 };
